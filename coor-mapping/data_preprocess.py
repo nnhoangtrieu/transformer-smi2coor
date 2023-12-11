a0a58ec0483a66688800892782c722b9b7fbab25
nnhoangtrieu <107376082+nnhoangtrieu@users.noreply.github.com>
2023-12-11 21:10:15 +0700

    clean folder

---
+++ coor-mapping/data_preprocess.py
@@ -0,0 +1,170 @@
+import torch
+import rdkit
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from helper import train_test_split
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Data: 
+    def __init__(self, smi_path, coor_path) :
+        self.smi_path = smi_path
+        self.coor_path = coor_path 
+        self.coor_list = self.get_coor()
+        self.smi_list = self.get_smi()
+
+        self.smi_dic = self.get_dic()
+
+        self.dic_len = len(self.smi_dic)
+        self.longest_smi = self.get_longest(self.smi_list)
+        self.longest_coor = self.get_longest(self.coor_list)
+
+        self.smint_list = self.get_smint()
+
+
+    def get_coor(self) :
+        all_coordinate = []
+        supplier = rdkit.Chem.SDMolSupplier(self.coor_path)
+        for mol in supplier:
+            coor_list = []
+            if mol is not None:
+                conformer = mol.GetConformer()
+                for atom in mol.GetAtoms():
+                    atom_idx = atom.GetIdx()
+                    x, y, z = conformer.GetAtomPosition(atom_idx)
+                    coor_atom = list((x,y,z))
+                    coor_list.append(coor_atom)
+            all_coordinate.append(coor_list)
+
+        return all_coordinate
+
+
+
+    def get_smi(self) :
+        smi_df = pd.read_csv(self.smi_path)
+        smi_list = list(smi_df['Canonical SMILES'])
+        smi_list = [smi + 'E' for smi in smi_list]
+
+        smi_list = self.replace_duplicate_atom(smi_list) 
+        return smi_list
+
+
+
+    def get_dic(self) :
+        smi_dic = {'x': 0, 'E': 1}
+        i = len(smi_dic)
+        for smi in self.smi_list :
+                for atom in smi :
+                    if atom not in smi_dic :
+                        smi_dic[atom] = i
+                        i += 1
+        return smi_dic
+
+
+
+    def get_longest(self, input_list) :
+        longest = 0
+        for i in input_list :
+            if len(i) > longest :
+                longest = len(i)
+        return longest
+
+
+
+
+
+
+    def get_smint(self) :
+        smint_list = []
+        for smi in self.smi_list :
+            smi = list(smi)
+            smint = [self.smi_dic[atom] for atom in smi]
+            smint = smint + [0] * (self.longest_smi - len(smint)) # Pad with 0
+            smint_list.append(smint)
+
+        return smint_list
+
+
+
+    def replace_duplicate_atom(self, smi_list) :
+        smi_list = [smi.replace('X', 'Na')
+                        .replace('Y', 'Cl')
+                        .replace('Z', 'Br')
+                        .replace('T', 'Ba') for smi in smi_list]
+
+        return smi_list
+
+
+
+
+    def extract(self) :
+        print(f"Train data extracted\nSize: {len(self.smint_list)}\nLongest SMILES: {self.longest_smi}\nLongest Coordinate: {self.longest_coor}")
+        print("----------------------------------------")
+        print(f"Sample x: {self.smint_list[0]}")
+        print(f"Sample y: {self.coor_list[0]}")
+        return self.smint_list, self.coor_list, self.smi_list, self.smi_dic, self.longest_smi, self.longest_coor
+
+
+
+class TorchDataset(Dataset) :
+  def __init__(self, x, y) :
+    self.x = x
+    self.y = y
+
+  def __len__(self) :
+    return len(self.x)
+
+  def __getitem__(self, idx) :
+    return torch.tensor(self.x[idx], dtype = torch.long, device=device), torch.tensor(self.y[idx], device = device)
+  
+
+
+def normalize_coor(coor_list) :
+    n_coor_list = []
+
+    for mol_coor in coor_list :
+        n_mol_coor = []
+
+        x_origin, y_origin, z_origin = mol_coor[0]
+
+        for atom_coor in mol_coor :
+            n_atom_coor = [round(atom_coor[0] - x_origin, 2), 
+                        round(atom_coor[1] - y_origin, 2), 
+                        round(atom_coor[2] - z_origin, 2)]
+            n_mol_coor.append(n_atom_coor)
+        n_coor_list.append(n_mol_coor)
+    return n_coor_list
+          
+
+
+def pad_coor(coor_list, longest_coor) :
+    p_coor_list = []
+
+    for i in coor_list :
+        if len(i) < longest_coor :
+            zeros = [[0,0,0]] * (longest_coor - len(i))
+            zeros = torch.tensor(zeros)
+            i = torch.tensor(i)
+            i = torch.cat((i, zeros), dim = 0)
+            p_coor_list.append(i)
+        else :
+            p_coor_list.append(i)
+    return p_coor_list
+
+
+
+# GET DATA
+smint_list, coor_list, smi_list, smi_dic, longest_smi, longest_coor = Data(smi_path='../data/smi_data.csv',
+                                                                           coor_path='../data/coor_data.sdf').extract()
+p_coor_list = pad_coor(coor_list, longest_coor)
+np_coor_list = pad_coor(normalize_coor(coor_list), longest_coor)
+train_x, train_y, test_x, test_y = train_test_split(x = smint_list, y = np_coor_list, ratio=0.95)
+
+
+
+B = 16
+train_set = TorchDataset(train_x, train_y)
+test_set = TorchDataset(test_x, test_y)
+train_loader = DataLoader(train_set, batch_size = B, shuffle = True)
+test_loader = DataLoader(test_set, batch_size = B)
---
+++ coor-mapping/helper.py
@@ -0,0 +1,178 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import time
+import math
+import rdkit
+from rdkit import Chem
+import torch
+plt.switch_backend('agg')
+
+
+
+def train_test_split(x, y, ratio = 0.9) :
+  ratio = int(len(x) * ratio)
+
+  train_x = x[:ratio]
+  train_y = y[:ratio]
+
+  test_x = x[ratio:]
+  test_y = y[ratio:]
+
+  return train_x, train_y, test_x, test_y
+
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+
+def count_atoms(smi):
+    # Parse the SMILES string
+    mol = Chem.MolFromSmiles(smi)
+
+    # Check if parsing was successful
+    if mol is not None:
+        # Count the number of atoms
+        num_atoms = mol.GetNumAtoms()
+        return num_atoms
+    else:
+        print("Error: Unable to parse SMILES string.")
+        return None
+
+
+
+# def visualize(attn, n_input, n_head, smi, output_path) :
+#     x, y = attn.size()
+#     attn = attn.cpu().numpy()
+    
+#     end_index = smi.index("E")
+#     smi = smi[:end_index]
+#     smi_len = len(smi)
+#     smi = [atom.replace('X', 'Na') for atom in smi]
+#     smi = [atom.replace('Y', 'Cl') for atom in smi]
+#     smi = [atom.replace('Z', 'Br') for atom in smi]
+#     smi = [atom.replace('T', 'Ba') for atom in smi]
+#     num_atom = count_atoms(''.join(smi))
+
+#     attn = attn[:num_atom, :smi_len]
+
+
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111)
+#     cax = ax.matshow(attn, cmap = 'viridis')
+#     fig.colorbar(cax)
+#     ax.set_xticklabels([''] + smi)
+#     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+#     ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+#     plt.savefig(f"{output_path}/Input {n_input +1} Head {n_head + 1}")
+#     plt.show()
+    
+
+
+def int2smi(input, invert_dic) :
+    output = []
+    input = input.cpu().numpy()
+    
+    for smiles in input :
+        out = [invert_dic[atom] for atom in smiles]
+        output.append(out)
+    return output
+
+
+
+def replace_duplicate_atom(smi) :
+    smi = list(smi[:-1])
+    smi = [smi.replace('Na', 'X')
+                    .replace('Cl', 'Y')
+                    .replace('Br', 'Z')
+                    .replace('Ba', 'T') for smi in smi]
+
+    return smi
+
+
+
+def smi2int(smi, smi_dic, longest_smi) :
+    smi = list(smi)
+    smint = [smi_dic[atom] for atom in smi]
+    smint = smint + [0] * (longest_smi - len(smint))
+    smint_torch = torch.tensor(smint).view(1,-1)
+
+    return smint_torch
+
+
+
+def evaluate(encoder, decoder, smi, smi_dic, longest_smi) :
+    encoder.eval()
+    decoder.eval()
+    
+    smint = smi2int(smi, smi_dic, longest_smi)
+
+    with torch.no_grad() :
+        e_out, e_last, self_attn = encoder(smint)
+        d_out, _, cross_attn = decoder(e_out, e_last)
+
+    cross_attn, self_attn = cross_attn.squeeze(), self_attn.squeeze()
+    cross_attn, self_attn = cross_attn.cpu().numpy(), self_attn.cpu().numpy()
+
+    return d_out, cross_attn, self_attn
+
+
+
+def plot_attn(matrix, smi, mode, path = "", name = "") :
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(matrix, cmap = "viridis")
+    fig.colorbar(cax)
+
+    if mode == "cross" :
+        ax.set_xticklabels([''] + smi)
+    if mode == "self" :
+        ax.set_xticklabels([''] + smi)
+        ax.set_yticklabels([''] + smi)
+    
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    
+    plt.savefig(f"{path}{name}.png")
+    plt.show()
+        
+
+
+
+def visualize(encoder,
+              decoder,
+              smi,
+              smi_dic,
+              longest_smi,
+              mode = "cross",
+              path = "",
+              name = 1) :
+    
+    _, cross_attn, self_attn = evaluate(encoder, decoder, smi, smi_dic, longest_smi)
+
+    # attn, self_attn = attn.squeeze(), self_attn.squeeze()
+    # attn, self_attn = attn.cpu().numpy(), self_attn.cpu().numpy()
+
+    smi = replace_duplicate_atom(smi)
+
+    coor_len = count_atoms(''.join(smi))
+    smi_len = len(smi)
+
+    if mode == "cross" :
+        matrix = cross_attn[:coor_len, :smi_len]
+    if mode == "self" :
+        matrix = self_attn[:smi_len, :smi_len]
+
+    plot_attn(matrix, smi, mode, path, name)
