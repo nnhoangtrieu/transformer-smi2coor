35e7700235dc6eac2da12544f7d402699203316b
nnhoangtrieu <107376082+nnhoangtrieu@users.noreply.github.com>
2023-12-12 16:17:58 +0700

    final update

--- transformer.py
+++ transformer.py
@@ -24,6 +24,11 @@
 
 # VISUAL_PATH : Name of folder to output attention image during training. I chose 5 random SMILES to output for each train.
 #             : I have already created a folder name "attention image"
+#             : The image is in the format H-R-CROSS-E. 
+#             : H1 means the attention of head number 1
+#             : R1 means the random SMILES number 1
+#             : CROSS or SELF means image of Cross Attention or Self Attention
+#             : E1 means the image of epoch number 1
 
 
 
